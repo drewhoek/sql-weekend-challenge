c41c7e46b31688687cc79ab957856363c4100f1c
Drew Hoek <drewh815@gmail.com>
2023-01-20 17:04:06 -0600

    GET request working, database info displayed on DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,32 @@
-$( function() {
+$(function () {
     console.log('jq and js');
+    getTasks();
 });
+
+function getTasks() {
+    console.log("in getTasks");
+    // ajax call to server to get koalas
+    $.ajax({
+        method: "GET",
+        url: "list/gettasks",
+    }).then(function (response) {
+        renderList(response);
+    });
+}
+
+function renderList(list) {
+    $("#list-body").empty();
+
+    console.log("in list history", list);
+
+    for (const item of list) {
+      $("#list-body").append(`
+    <tr> 
+    <td> ${item.task}</td>
+    <td> ${item.complete} </td>
+    <td> ${item.timeCompleted} </td>
+    <td> <button data-id="${item.id}" class="delete-btn"> DELETE </button></td>
+    <td> <button data-id="${item.id}" class="edit-btn"> EDIT </button></td>
+    </tr>`);
+    }
+}
--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -2,5 +2,21 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool");
 
+// GET request to aquire data from database
+router.get("/gettasks", (req, res) => {
+    console.log("router list");
+    const queryText = `SELECT * FROM "list";`;
+
+    pool
+        .query(queryText)
+        .then((result) => {
+            console.log("successful select from database");
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`error ${queryText}`, error);
+            res.sendStatus(505);
+        });
+});
 
 module.exports = router;
