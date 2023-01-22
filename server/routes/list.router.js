f1d745cd3a932776ef075203d66fc9103c377770
Drew Hoek <drewh815@gmail.com>
2023-01-21 23:08:34 -0600

    Server side PUT request working as intended

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -62,4 +62,16 @@ router.delete('/:id', (req, res) => {
     }
 });
 
+router.put('/:id', (req, res) => {
+    console.log(`Updating task with id ${req.params}`);
+    let queryText = `UPDATE "list" SET "complete"= 'true' WHERE id=$1 RETURNING *;`;
+    pool.query(queryText, [req.params.id]).then((result) => {
+      res.send(result.rows);
+    })
+    .catch(error => {
+      console.log('error in editomg list', error);
+      res.sendStatus(500);
+    });
+  });
+
 module.exports = router;
